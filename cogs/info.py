0b423d7bb1f139675ce64bf80b824c8b2ae96ea6
Cytech <22370499+DevCytech@users.noreply.github.com>
2025-01-14 22:24:18 -0500

    Small but More Info
    Continued working on information commands
    
    - Finished guild info command by adding events, and vanity url
    - Created channel info command
    - Created bot info command with some basic text

--- cogs/info.py
+++ cogs/info.py
@@ -1,9 +1,7 @@
 import discord
+import textwrap
 from discord import app_commands
 from discord.ext import commands
-
-# TO DO 
-# Guild Info --- Scheduled Events, vanity invite, language
 
 
 class InfoCommands(commands.Cog):
@@ -15,6 +13,15 @@ class InfoCommands(commands.Cog):
         print(f'{__name__} has loaded and is ready')
 
     # Bot Info
+    @app_commands.command(name='bot-info', description='Get information about me!')
+    async def botinfo(self, interaction: discord.Integration):
+        bot_info_embed = discord.Embed(title='About Me', description='Hello I am Project Kyoto, an Discord bot to help make your life easier throughout the day. So far my abilities are a little underwhelming but I am getting there slowly, please bear with me! I was started as a passion project, and will hopefully stay that way. I hope you enjoy the functions I currently have and will enjoy using me as I grow bigger! Thank you so much for the support both from me and the Development team powering this project. If you need any help please read below, as always, have a great day!', color=discord.Color.purple())
+        bot_info_embed.add_field(name='Help', value='If you need general help please view my `/help` or visit my website and go to the FAQ page!', inline=False)
+        bot_info_embed.add_field(name='Commands', value='To view all of my commands please use `/command-list` or view them in the `/` area!', inline=False)
+        bot_info_embed.add_field(name='Website', value='https://disconnectbot.com/project-kyoto', inline=False)
+        bot_info_embed.set_thumbnail(url='https://images-ext-1.discordapp.net/external/odub4mNDY9mIiHXLzu12sgohpMWaXxuD5hHvUcZfO2c/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/1136826536377458800/fa55e685ee4c99b1356af57be915187a.png?format=webp&quality=lossless&width=636&height=636')
+
+        await interaction.response.send_message(embed=bot_info_embed)
 
     # Help Command
 
@@ -23,6 +30,27 @@ class InfoCommands(commands.Cog):
     # User Info
 
     # Channel Info
+    @app_commands.command(name='channel-info', description='Get information about your current channel or specifed channel')
+    @app_commands.guild_only()
+    async def channelinfo(self, interaction: discord.Interaction, textchannel: discord.TextChannel = None):
+        fallback_channel = interaction.channel
+        if textchannel is not None:
+            target_channel = textchannel
+        else:
+            target_channel = fallback_channel
+
+        channel_info_embed = discord.Embed(title=f'{target_channel.name} Information', description=f'{target_channel.topic if not 'None' else 'This channel does not have a topic listed'}', color=discord.Color.pink())
+        channel_info_embed.add_field(name='Category', value=f'{target_channel.category if not 'None' else 'Not in a Category'}', inline=True)
+        channel_info_embed.add_field(name='Members Allowed', value=f'🙋‍♂️ {len(target_channel.members)}', inline=True)
+        channel_info_embed.add_field(name='Thread Count', value=f'💬 {len(target_channel.threads)}', inline=True)
+
+        if target_channel.is_nsfw():
+           channel_info_embed.add_field(name='NSFW', value='🔞 This channel is considered an NSFW channel.', inline=True)
+
+        channel_info_embed.add_field(name='Jump Link', value=f'🔗 Jump to this channel: {target_channel.jump_url}', inline=False)
+        channel_info_embed.set_footer(text=f'Channel ID: {target_channel.id}')
+
+        await interaction.response.send_message(embed=channel_info_embed)
 
     # Guild Info
     @app_commands.command(name='guild-info', description='Get the information and statistics for your current guild!')
@@ -54,6 +82,22 @@ class InfoCommands(commands.Cog):
             # Conditional elements
             if guild.icon is not None: guild_info_embed.set_thumbnail(url=guild.icon)
             if guild.banner is not None: guild_info_embed.set_image(url=guild.banner)
+
+            if guild.vanity_url is not None: guild_info_embed.add_field(name='Vanity URL', value=f'{guild.vanity_url}')
+
+            if len(guild.scheduled_events) > 0:
+                i = 0
+                events = ''
+                for e in guild.scheduled_events:
+                    if i == 5: break
+                    i += 1
+                    if i == 1:
+                        events = f'[{e.name}]({e.url}) on {e.start_time.strftime('%m-%d-%Y at %H:%M')}. {textwrap.shorten(e.description, width=75, placeholder='') if not None else ''}..'
+                    else:
+                        events = f'{events} \n[{e.name}]({e.url}) on {e.start_time.strftime('%m-%d-%Y at %H:%M')}. {textwrap.shorten(e.description, width=75, placeholder='') if not None else ''}..'
+
+
+                guild_info_embed.add_field(name='Upcoming events:', value=f'{events}', inline=False)
 
             await interaction.response.send_message(embed=guild_info_embed)
         except Exception as e:
